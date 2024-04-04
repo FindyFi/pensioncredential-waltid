2ed81152bc8fdedc573dcfd6ca4551dee3498b48
Samuel Rinnetmäki <samuel@iki.fi>
2024-04-04 15:16:21 +0300

    support multiple example credentials

--- issuer.js
+++ issuer.js
@@ -1,9 +1,7 @@
 import { createServer } from 'node:http'
-import pensionCredential from './pensioncredential.json' assert {'type': 'json'}
 import { config, roles } from './init.js'
 
 // console.log(roles)
-// console.log(JSON.stringify(pensionCredential, null, 2))
 
 async function getHolderDid() {
   const headers = {
@@ -16,13 +14,15 @@ async function getHolderDid() {
   return holder
 }
 
-async function getOffer() {
+async function getOffer(path) {
   const holder = await getHolderDid()
   const issueUrl = `${config.issuer_api}/openid4vc/sdjwt/issue`
+  const { default: credential } = await import('.' + path, { assert: { type: "json" } });
+  console.log(credential)
   const requestBody = {
     "issuanceKey": JSON.parse(roles.issuer.key),
     "issuerDid": roles.issuer.did,
-    "vc": pensionCredential,
+    "vc": credential,
     "mapping": {
       "id": "<uuid>",
       "issuer": {
@@ -43,29 +43,18 @@ async function getOffer() {
           "children": {
             "fields": {
               "Pension": {
-                "sd": false,
-                "children": {
-                  "fields": {
-                    "endDate": { "sd": true },
-                    "startDate": { "sd": true },
-                    "statusCode": { "sd": true },
-                    "typeCode": { "sd": true },
-                    "typeName": { "sd": true },
-                  }
-                }
+                "sd": true,
               },
               "Person": {
                 "sd": false,
                 "children": {
                   "fields": {
-                    "birthDate": { "sd": true },
-                    "familyName": { "sd": true },
-                    "givenName": { "sd": true },
+                    "person_identifier_code": { "sd": true },
+                    "birth_date": { "sd": true },
+                    "family_name_national_characters": { "sd": true },
+                    "given_name_national_characters": { "sd": true },
                   }
                 }
-              },
-              "IdentityObject": {
-                "sd": true,
               }
             }
           }
@@ -82,7 +71,7 @@ async function getOffer() {
     body: JSON.stringify(requestBody)
   }
   // console.log(JSON.stringify(pensionCredential, null, 1))
-  // console.log(issueUrl, JSON.stringify(credParams, null, 1))
+  console.log(issueUrl, JSON.stringify(credParams, null, 1))
   const resp = await fetch(issueUrl, credParams)
   const credentialOffer = await resp.text()
   // console.log(resp.status, credentialOffer)
@@ -91,7 +80,21 @@ async function getOffer() {
 
 const sendOffer = async function (req, res) {
   const path = new URL(`http://${config.server_host}${req.url}`).pathname
-  if (path !== '/') {
+  if (path.includes('.json')) {
+    res.setHeader("Content-Type", "text/plain")
+    try {
+      const offerUri = await getOffer(path)
+      res.writeHead(200)
+      res.end(offerUri)
+      return false
+    }
+    catch(e) {
+      res.writeHead(404)
+      res.end(`${path} not found!`)
+      return false  
+    }
+  }
+  else if (path !== '/') {
     res.setHeader("Content-Type", "text/plain")
     res.writeHead(404)
     res.end(`Not Found`)
@@ -111,36 +114,57 @@ const sendOffer = async function (req, res) {
  </style>
  <body style="text-align: center;">
   <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Kela_suomi_kela-1-.jpg/220px-Kela_suomi_kela-1-.jpg" alt="Kela" />
-  <h1>Heippa vahvasti tunnistettu asiakas!</h1>
-  <p>Skannaapa oheinen QR-koodi digikukkarollasi niin laitetaan sinne eläketodistetta tulemaan...</p>
+  <h1>Heippa asiakas!</h1>
+  <p id="instructions">Tunnistaudu palveluun valitsemalla henkilöllisyytesi valintalistasta:</p>
+  <form id="idSelector">
+   <select name="identity">
+   <option value="pensioncredential.json">Totti Aalto</option>
+   <option value="pensioncredential-provisional.json">Matti Meikäläinen</option>
+   <option value="pensioncredential-disability.json">Joni Kai Hiltunen</option>
+   <option value="pensioncredential-rehabilitation.json">Jonne Aapeli Setälä</option>
+   <option value="pensioncredential-rehabilitation-expired.json">Annina von Forsellestes</option>
+   </select>
+   <input type="submit" value="Vahvista" />
+  </form>
   <canvas id="qrcode"></canvas>
   <p id="offer">Valmistellaan...</p>
   <script>
-   let qrUrl = '${await getOffer()}'
-   const canvas = document.getElementById("qrcode");
-   const qr = QRCode.QRCodeBrowser(canvas);
-   qr.setOptions({
-     text: qrUrl,
-     size: 256,
-   })
-   qr.draw()
+
    const a = document.createElement('a')
    a.textContent = 'Kopioi todistetarjous leikepöydälle.'
-   a.href = qrUrl
-   a.onclick = function(e) {
-    e.preventDefault()
-    try {
-     navigator.clipboard.writeText(this.href)
-    } catch (error) {
-     console.error(error.message)
-    }
-   }
-   // document.querySelector('#qrcode').onnoclick = () => {document.location.href = qrUrl}
    const o = document.querySelector('#offer')
-   o.textContent = ''
-   o.appendChild(a)
+
+   const f = document.querySelector('#idSelector')
+   f.onsubmit = async function(e) {
+    e.preventDefault()
+    const file = this.identity.value
+    console.log(file)
+    const resp = await fetch(file)
+    let qrUrl = await resp.text()
+    console.log(qrUrl)
+    const canvas = document.getElementById("qrcode")
+    const qr = QRCode.QRCodeBrowser(canvas)
+    qr.setOptions({
+      text: qrUrl,
+      size: 256,
+    })
+    qr.draw()
+    document.querySelector('#instructions').textContent = 'Skannaapa oheinen QR-koodi digikukkarollasi niin laitetaan sinne eläketodiste tulemaan...'
+    a.href = qrUrl
+    a.onclick = function(e) {
+     e.preventDefault()
+     try {
+      navigator.clipboard.writeText(this.href)
+     } catch (error) {
+      console.error(error.message)
+     }
+    }
+    // document.querySelector('#qrcode').onnoclick = () => {document.location.href = qrUrl}
+    o.textContent = ''
+    o.appendChild(a)
+   }
   </script>
-  </body>
+ </body>
 </html>`)
 }
 
